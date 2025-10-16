e6d87319ab972e773d3aacc4e9706cd8150a48c1
dobiedad <leomdiv@gmail.com>
2025-10-16 13:31:46 +0100

    tidy

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,13 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters
+
     protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:profile_image])
+    end
 
     def after_update_path_for(_resource)
       profile_path
