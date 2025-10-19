da13f9ca963d92085cd9df9de0a1cce0b22e27ed
dobiedad <leomdiv@gmail.com>
2025-10-19 18:11:12 +0100

    format currency

--- app/javascript/controllers/ticket_purchase_controller.js
+++ app/javascript/controllers/ticket_purchase_controller.js
@@ -5,13 +5,14 @@ export default class extends Controller {
 
   connect() {
     this.ticketPrice = parseFloat(this.element.dataset.ticketPrice) || 0
+    this.currencySymbol = this.element.dataset.currencySymbol || '$'
     this.updateTotal()
   }
 
   updateTotal() {
     const quantity = parseInt(this.quantityTarget.value) || 1
     const total = (this.ticketPrice * quantity).toFixed(2)
-    this.totalTarget.textContent = `$${total}`
+    this.totalTarget.textContent = `${this.currencySymbol}${total}`
     
     if (this.hasButtonTarget) {
       const ticketWord = quantity === 1 ? 'Ticket' : 'Tickets'
--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module RaffleShop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.x.currency_symbol = '$'
+    config.x.currency_unit = '$'
   end
 end
