8deabcdfcab1fed473fc61a6f1de05fefb6453d1
dobiedad <leomdiv@gmail.com>
2025-10-19 08:48:36 +0100

    add empty state component

---
+++ app/components/ui/empty_state.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module UI
+  class EmptyState < ApplicationViewComponent
+    attribute :icon, :string
+    attribute :title, :string
+    attribute :description, :string
+    attribute :action
+  end
+end
