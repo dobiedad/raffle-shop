52631f3833f3df866259aa1055357d43a119b282
dobiedad <leomdiv@gmail.com>
2025-10-21 14:22:21 +0100

    rubo

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,8 +7,8 @@ module Users
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_image])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name profile_image])
     end
 
     def after_update_path_for(_resource)
