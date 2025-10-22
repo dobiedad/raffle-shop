6cce40c42e17ed158ece92337993acf83b171ea2
dobiedad <leomdiv@gmail.com>
2025-10-22 09:31:46 +0100

    stupid cursor

--- app/components/ui/social_icons.rb
+++ app/components/ui/social_icons.rb
@@ -10,19 +10,6 @@ module UI
     attribute :show_twitter, default: true
     attribute :show_email, default: true
 
-    def initialize(share_url:, share_text: 'Check this out!', show_native_share: true,
-                   show_whatsapp: true, show_telegram: true, show_twitter: true,
-                   show_email: true, **options)
-      super
-      @share_url = share_url
-      @share_text = share_text
-      @show_native_share = show_native_share
-      @show_whatsapp = show_whatsapp
-      @show_telegram = show_telegram
-      @show_twitter = show_twitter
-      @show_email = show_email
-    end
-
     def whatsapp_url
       "https://wa.me/?text=#{ERB::Util.url_encode("#{share_text}: #{share_url}")}"
     end
